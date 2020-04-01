66fdc4de3b216c8637078937a7be736c2d0dafec
oleoot <oleoot28@gmail.com>
2020-04-01 23:48:19 +0300

    First commit

---
+++ to-do-list/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App() {
+  constructor(props){
+    super(props);
+
+  }
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
